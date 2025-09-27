ba3f8bc5cac7c4f35ea45b8ec7314c8b5368608f
Kalpesh Parmar <kalpesh2112004@gmail.com>
2025-09-27 09:01:01 +0530

    get all videos function completed

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,9 +1,98 @@
-import mongoose, {isValidObjectId} from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-const getAllVideos = asyncHandler(async() => {
+const getAllVideos = asyncHandler(async (req, res) => {
+    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+    console.log(userId);
+    const pipeline = []
 
+    // for searching
+    if (query) {
+        pipeline.push({
+            $match: {
+                $text: {
+                    $search: query
+                }
+            }
+        })
+
+        pipeline.push({
+            $addFields: {
+                score: { $meta: "textScore" }
+            }
+        })
+
+        pipeline.push({
+            $sort: { score: -1 }
+        })
+    }
+
+    if (userId) {
+        if (!isValidObjectId(userId)) {
+            throw new ApiError(400, "Invalid userId")
+        }
+
+        pipeline.push({
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        })
+    }
+
+    // fetch videos only that are set isPublished as true
+    pipeline.push({ $match: { isPublished: true } })
+
+    //sortBy can be views, createdAt, duration
+    //sortType can be ascending(-1) or descending(1)
+    if (sortBy && sortType) {
+        pipeline.push({
+            $sort: {
+                [sortBy]: sortType === "asc" ? 1 : -1
+            }
+        })
+    } else {
+        pipeline.push({
+            $sort: { createdAt: -1 }
+        })
+    }
+
+    pipeline.push(
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "ownerDetails",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            "avatar.url": 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $unwind: "$ownerDetails"
+        }
+    )
+
+    const videoAggregate = Video.aggregate(pipeline)
+
+    const options = {
+        page: parseInt(page, 10),
+        limit: parseInt(limit, 10)
+    }
+
+    const video = await Video.aggregatePaginate(videoAggregate, options)
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, video, "Videos fetched successfully"))
 })
 
 export {
