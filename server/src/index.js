167c900d65dba1e7fd34095709ac99683f54164f
Kalpesh Parmar <kalpesh2112004@gmail.com>
2025-10-04 14:53:35 +0530

    client folder setup for react and frontend

--- server/src/index.js
+++ server/src/index.js
@@ -3,17 +3,15 @@ import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
 dotenv.config({
-  path: "./.env",
+    path: "./.env",
 });
 
 connectDB()
-  .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
-
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
     })
-  })
-  .catch((err) => {
-    console.log("MONGODB connection failed !! ", err);
-
-  })
+    .catch((err) => {
+        console.log("MONGODB connection failed !! ", err);
+    });
