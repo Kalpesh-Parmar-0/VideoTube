e4b7438932dd37c919606649883b1c6180ceca3b
Kalpesh Parmar <kalpesh2112004@gmail.com>
2025-10-06 08:39:20 +0530

    redux store and slice create

---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+import Button from './Button';
+import Container from './Container';
+import Input from './Input';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar'
+
+export {
+    Button,
+    Container,
+    Input,
+    Navbar,
+    Sidebar,
+}
