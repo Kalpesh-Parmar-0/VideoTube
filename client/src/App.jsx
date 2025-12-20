6f9d6ada311c48d37aeb70019614ad4a884ba097
Kalpesh Parmar <kalpesh2112004@gmail.com>
2025-12-20 15:55:21 +0530

    skeleton for login and home done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,9 +27,9 @@ function App() {
           <Route path='' element={<HomePage />} />
           <Route path='/my-content' element={<MyChannel />}>
             <Route path='videos' element={<MyChannelVideos />} />
-            <Route path='playlists' element='' />
-            <Route path='tweets' element='' />
-            <Route path='subscribed' element='' />
+            <Route path='playlists' element={''} />
+            <Route path='tweets' element={''} />
+            <Route path='subscribed' element={''} />
           </Route>
         </Route>
       </Routes>
