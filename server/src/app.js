f2383ff577e4469c7318d5296c65152bfcdc9d88
Kalpesh Parmar <kalpesh2112004@gmail.com>
2025-12-29 11:29:27 +0530

    subscription router and controller completed

--- server/src/app.js
+++ server/src/app.js
@@ -23,6 +23,7 @@ import videoRouter from "./routes/video.routes.js"
 import dashboardRouter from "./routes/dashboard.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
@@ -30,5 +31,6 @@ app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
 app.use("/api/v1/comment", commentRouter)
 app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
 
 export { app };
---
+++ server/src/routes/subscription.routes.js
@@ -0,0 +1,15 @@
+import {Router} from "express"
+import {
+    getSubscribedChannels,
+    getUserChannelSubscribers,
+    toggleSubscription
+} from "../controllers/subscription.controller.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+
+const router = Router()
+router.use(verifyJWT)
+
+router.route("/channel/:channelId").get(getUserChannelSubscribers).post(toggleSubscription)
+router.route("/users/:subscriberId").get(getSubscribedChannels)
+
+export default router
