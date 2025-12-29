9e5d43c60d2105832ed561b53fdae76448d12703
Kalpesh Parmar <kalpesh2112004@gmail.com>
2025-12-29 09:25:36 +0530

    comment slice complete

--- client/src/store/slices/commentSlice.js
+++ client/src/store/slices/commentSlice.js
@@ -7,3 +7,72 @@ const initialState = {
     loading: false,
     comments: []
 }
+
+export const createAcomment = createAsyncThunk("createAcomment", async({videoId, content})=> {
+    try {
+        const res = await axiosInstance.post(`/comment/${videoId}`, content)
+        console.log(res.data.data);
+        return res.data.data
+    } catch (error) {
+        toast.error(error?.response?.data?.error)
+        throw error
+    }
+})
+
+export const editAcomment = createAsyncThunk("editAcomment", async({commnetId, content})=> {
+    try {
+        const res = await axiosInstance.patch(`/comment/channel/${commnetId}`, content)
+        console.log(res.data.data)
+        return res.data.data
+    } catch (error) {
+        toast.error(error?.response?.data?.error)
+        throw error
+    }
+})
+
+export const deleteAcomment = createAsyncThunk("deleteAcomment", async({commnetId})=> {
+    try {
+        const res = await axiosInstance.delete(`/comment/channel/${commnetId}`)
+        console.log(res.data.data)
+        return res.data.data
+    } catch (error) {
+        toast.error(error?.response?.data?.error)
+        throw error
+    }
+})
+
+export const getVideoComments = createAsyncThunk("getVideoComments", async({videoId, page, limit})=> {
+
+    const url = new URL(`${BASE_URL}/${videoId}`)
+    if (page) url.searchParams.set('page',page)
+    if (limit) url.searchParams.set('limit', limit)
+
+    try {
+        const res = await axiosInstance.get(url)
+        console.log(res.data.data)
+        return res.data.data
+    } catch (error) {
+        toast.error(error?.response?.data?.error)
+        throw error
+    }
+})
+
+const commentSlice = createSlice({
+    name: "comment",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(getVideoComments.pending, 
+            (state)=> (state.loading = true)
+        )
+
+        builder.addCase(getVideoComments.fulfilled,
+            (state, action) => {
+                state.loading = false
+                state.comments = action.payload
+            }
+        )
+    }
+})
+
+export default commentSlice.reducer
