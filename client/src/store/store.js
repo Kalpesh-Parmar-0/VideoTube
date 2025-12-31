cbd6a68c5b9f2c146416dfe8f87f41f41d699f2d
Kalpesh Parmar <kalpesh2112004@gmail.com>
2025-12-31 18:21:53 +0530

    store updated

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,6 +3,10 @@ import authSliceReducer from "./slices/authSlice.js";
 import uiReducer from "./slices/uiSlice.js"
 import userSliceReducer from "./slices/userSlice.js";
 import videoSliceReducer from "./slices/videoSlice.js";
+import subscriptionSlice from "./slices/subscriptionSlice.js";
+import likeSlice from "./slices/likeSlice.js";
+import tweetSlice from "./slices/tweetSlice.js";
+import commentSlice from "./slices/commentSlice.js";
 
 const store = configureStore({
     reducer: {
@@ -10,6 +14,10 @@ const store = configureStore({
         ui: uiReducer,
         user: userSliceReducer,
         video: videoSliceReducer,
+        subscription: subscriptionSlice,
+        like: likeSlice,
+        tweet: tweetSlice,
+        comment: commentSlice,
     },
 });
 
